d786dfba476228bf11fa940d9845b6ccf5f2770e
skyecodes <skyecodes@proton.me>
2025-10-01 21:14:09 +0200

    feat: migrate to architectury

---
+++ forge/src/main/java/dev/franckyi/pwah/forge/mixin/BoneMealItemMixin.java
@@ -0,0 +1,19 @@
+package dev.franckyi.pwah.forge.mixin;
+
+import dev.franckyi.pwah.Pwah;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BoneMealItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(BoneMealItem.class)
+public class BoneMealItemMixin {
+    @Redirect(method = "useOnBlock", at = @At(target = "Lnet/minecraft/item/BoneMealItem;applyBonemeal(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;)Z", value = "INVOKE"))
+    public boolean stuff(ItemStack stack, World world, BlockPos pos, PlayerEntity player) {
+        return world.getBlockState(pos).getBlock() != Pwah.PWAH_BLOCK && BoneMealItem.applyBonemeal(stack, world, pos, player);
+    }
+}
